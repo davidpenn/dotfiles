d78c87e238c901c09f6523cfca4155bfa0713280
David Penn <david.penn@me.com>
2016-04-01 20:42:41 +0200

    moving osx-init to another repository

--- .osx-init/homebrew.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install caskroom/cask/brew-cask
-brew tap caskroom/versions
--- .osx-init/setup-atom.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-apm install go-plus language-docker minimap pretty-json atom-material-ui
